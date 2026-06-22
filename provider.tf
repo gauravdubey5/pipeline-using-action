841c485e1d242213944241488b2d6a75cfc6207e
Gaurav S <gaurav7860591437@gmail.com>
2026-06-22 22:52:02 +0530

    Add Azure resource group configuration

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.73.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_groups" {
+
+  name     = orange-rg
+  location = centralus
+
+}
