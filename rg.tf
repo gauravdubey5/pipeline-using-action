65600dcdd9370e2e9777dce902eabdf26b6b5f5c
Gaurav S <gaurav7860591437@gmail.com>
2026-06-30 13:58:15 +0530

    Rename resource groups in rg.tf

--- rg.tf
+++ rg.tf
@@ -1,8 +1,8 @@
-resource "azurerm_resource_group" "resource_groups" {
+resource "azurerm_resource_group" "resource_groups1" {
   name     = "gauravaction-rg1"
   location = "centralindia"
 }
-resource "azurerm_resource_group" "resource_groups" {
+resource "azurerm_resource_group" "resource_groups2" {
   name     = "gauravaction-rg2"
   location = "centralindia"
 }
