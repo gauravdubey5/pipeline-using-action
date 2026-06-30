bc2b8c2f1ebc62a247887411d7b030f146eda947
Gaurav <gaurav7860591437@gmail.com>
2026-06-30 14:05:16 +0530

    first

--- rg.tf
+++ rg.tf
@@ -10,3 +10,7 @@ resource "azurerm_resource_group" "resource_groups3" {
   name     = "gauravaction-rg3"
   location = "centralindia"
 }
+resource "azurerm_resource_group" "resource_groups4" {
+  name     = "gauravaction-rg4"
+  location = "centralindia"
+}
